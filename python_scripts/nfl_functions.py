c7aaa9c55b2530bd9448a9437f4686958add41cc
Paul Clanon <pac@well.com>
2022-07-20 17:00:11 -0700

    Changes and additions.

---
+++ python_scripts/nfl_functions.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+
+df = pd.read_csv('/Users/paulclanon/Documents/NFL_2022/2022_NFL_CBCL.csv')
+df = df[df['WEEK'] == 1]
+
+
+def this_week_matchups(df):
+    this_week_matchups = tuple(zip(df.index.tolist(), df['ROAD TEAM'], df['HOME TEAM']))
+
+    return this_week_matchups
+
+if __name__ == '__main__':
+    this_week_matchups(df)
