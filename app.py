4f2dd20cf4d255d3329be0e05c457bf014cd3165
Paul Clanon <pac@well.com>
2022-07-22 15:16:55 -0700

    Changes and additions.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+import flask_wtf
 from flask_bootstrap import Bootstrap
+from werkzeug.utils import secure_filename
 import datetime
-from python_scripts import giants_dashboard, almanac_items
+import pandas as pd
+from python_scripts import giants_dashboard, almanac_items, nfl_functions
 
+
+allsky_uploads_path = '/Users/paulclanon/Documents/Python_Scripts/PycharmProjects/paulclanon_com/static/img/allsky/daily_uploads/'
 
 def create_app():
     app = Flask(__name__)
@@ -10,13 +17,29 @@ def create_app():
 
     return app
 
-
-# app = Flask(__name__)
 app = create_app()
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = '2lospalmos'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 @app.route('/baseball')
@@ -73,6 +96,35 @@ def music():
 def allsky():
     return render_template('allsky/allsky.html')
 
+@app.route('/allsky_uploads', methods=['GET', 'POST'])
+def allsky_uploads():
+
+    if request.method == 'POST':
+        if 'allsky_upload' not in request.files:
+            return redirect(request.url)
+        file = request.files['allsky_upload']
+
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file.filename.lower().endswith(('.jpg', '.mp4')):
+            filename = secure_filename(file.filename)
+            file.save(f'{allsky_uploads_path}{filename}')
+            return redirect(request.url)
+    return '''
+            <h1>Upload</h1>
+            <form method="post" enctpye="multipart/form-data">
+            <input type="file" name="file">
+            <input type="submit" name="Upload">
+        '''
+@app.route('/cbcl/mypicks', methods=['GET','POST'])
+def mypicks():
+    df = pd.read_csv('/Users/paulclanon/Documents/NFL_2022/2022_NFL_CBCL.csv')
+    df = df[df['WEEK'] == 17]
+    this_week_matchups = nfl_functions.this_week_matchups(df)
+    print(request.form)
+    print(request.form.get('player'))
+    return render_template('cbcl/mypicks.html', this_week_matchups=this_week_matchups)
 
 if __name__ == "__main__":
     app.run()
