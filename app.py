a2f3382a3f76267a3180efd47752688438d0446c
Paul Clanon <pac@well.com>
2022-07-26 11:59:01 -0700

    Changes and additions.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-import flask_wtf
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
+import flask_monitoringdashboard as dashboard
 import datetime
 import pandas as pd
 from python_scripts import giants_dashboard, almanac_items, nfl_functions
@@ -14,18 +17,46 @@ allsky_uploads_path = '/Users/paulclanon/Documents/Python_Scripts/PycharmProject
 def create_app():
     app = Flask(__name__)
     Bootstrap(app)
+    dashboard.bind(app)
 
     return app
 
 app = create_app()
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = '2lospalmos'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': "Password"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError('That username is already taken.')
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': "Username"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
+                           render_kw={'placeholder': "Password"})
+    submit = SubmitField('Login')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(username=username.data).first()
+        if existing_user_username:
+            raise ValidationError('That username is already taken.')
+
 
 @app.route('/')
 def index():
