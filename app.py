fa66804b30df760d069d10adc7491a9fce8a8b99
Paul Clanon <pac@well.com>
2022-08-01 16:36:56 -0700

    Changes and additions.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect
-from flask_login import UserMixin
+# import bcrypt as bcrypt
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
 import flask_monitoringdashboard as dashboard
@@ -26,6 +28,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = '2lospalmos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt_obj = Bcrypt(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,28 +45,27 @@ class User(db.Model, UserMixin):
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
-                           render_kw={'placeholder': "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
-                           render_kw={'placeholder': "Password"})
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
     submit = SubmitField('Register')
 
     def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
         if existing_user_username:
-            raise ValidationError('That username is already taken.')
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[InputRequired(), Length(min=4, max=20)],
                            render_kw={'placeholder': "Username"})
-    password = PasswordField(validators=[InputRequired(), Length(min=4, max=20)],
+    password = PasswordField(validators=[InputRequired(), Length(min=8, max=20)],
                            render_kw={'placeholder': "Password"})
     submit = SubmitField('Login')
-
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(username=username.data).first()
-        if existing_user_username:
-            raise ValidationError('That username is already taken.')
 
 
 @app.route('/')
@@ -63,14 +73,44 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt_obj.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('login_dashboard'))
+
+    return render_template('login.html', form=form)
 
 
-@app.route('/register')
+@app.route('/login_dashboard', methods=['GET', 'POST'])
+@login_required
+def login_dashboard():
+    return render_template('login_dashboard.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt_obj.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 
 @app.route('/baseball')
@@ -149,12 +189,14 @@ def allsky_uploads():
             <input type="submit" name="Upload">
         '''
 @app.route('/cbcl/mypicks', methods=['GET','POST'])
+@login_required
 def mypicks():
     df = pd.read_csv('/Users/paulclanon/Documents/NFL_2022/2022_NFL_CBCL.csv')
     df = df[df['WEEK'] == 17]
     this_week_matchups = nfl_functions.this_week_matchups(df)
+    print(current_user.username)
     print(request.form)
-    print(request.form.get('player'))
+
     return render_template('cbcl/mypicks.html', this_week_matchups=this_week_matchups)
 
 if __name__ == "__main__":
